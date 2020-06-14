8a01279e37b4478a2eca8b5ad45f7b3abaa1750b
yukarinoki <blkheartpg@gmail.com>
2020-06-15 00:50:03 +0900

    a

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
-git clone https://github.com/google/fuzzing.git fuzzing
-git clone https://github.com/google/fuzzer-test-suite.git FTS
 sh ./fuzzing/tutorial/libFuzzer/install-deps.sh
 sh ./fuzzing/tutorial/libFuzzer/install-clang.sh
