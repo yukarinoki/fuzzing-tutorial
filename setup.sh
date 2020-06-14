16fe5e3cac23665cc21f210f7d2facae7a060014
yukarinoki <blkheartp@gmail.com>
2020-06-14 06:25:57 +0000

    setup prebuild

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/google/fuzzing.git fuzzing
+git clone https://github.com/google/fuzzer-test-suite.git FTS
+sh ./fuzzing/tutorial/libFuzzer/install-deps.sh
+sh ./fuzzing/tutorial/libFuzzer/install-clang.sh
